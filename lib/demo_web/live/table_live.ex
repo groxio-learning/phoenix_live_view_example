7206ff6be1e7158884f6e814a218f8f4bc21f1c5
Bruce Tate <bruce@rapidred.com>
2019-04-03 08:51:16 -0400

    liveview fixes, table tweaks

--- lib/demo_web/live/table_live.ex
+++ lib/demo_web/live/table_live.ex
@@ -3,60 +3,73 @@ defmodule DemoWeb.TableLive do
 
   def render(assigns) do
     ~L"""
-    <a>Counter 1</a> | <a>Counter 2</a>
-    <hr/>
-    <form>
-      <input type="text" name="q" placeholder="Search..." />
-    </form>
+    <div>
+      <a>Counter 1</a> | 
+      <a>Counter 2</a>
+      <hr/>
+      <form>
+        <input 
+          type="text" 
+          name="q" 
+          placeholder="Search..." 
+        />
+      </form>
 
-    <%= table(assigns) %>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Value</th>
+        </tr>
+        <tr>
+          <td>Socket.assigns.hello</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Ticker</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Counter1</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Counter2</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Search Field type</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Search Field submit</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Search Field focus</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Search Field blur</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Key up</td>
+          <td>?</td>
+        </tr>
+        <tr>
+          <td>Key down</td>
+          <td>?</td>
+        </tr>
+      </table>
+    </div>
     """
   end
   
-  def table(assigns) do
-    ~L"""
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Value</th>
-      </tr>
-      <tr>
-        <td>Socket.assigns.hello</td>
-        <td><%= @hello %></td>
-      </tr>
-      <tr>
-        <td>System Counter</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Counter1</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Counter2</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Search Field type</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Search Field submit</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Search Field focus</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Search Field blur</td>
-        <td>?</td>
-      </tr>
-    </table>
-    """
-  end
-
   def mount(_session, socket) do
-    {:ok, assign(socket, :hello, :world)}
+    {
+      :ok, 
+      socket
+    }
   end
 end
