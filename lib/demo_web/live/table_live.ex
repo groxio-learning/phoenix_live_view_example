fe3cc64ac10eb4382d5057287346a76aa7d60522
Bruce Tate <bruce@rapidred.com>
2019-04-05 16:37:16 -0400

    final form: demo

--- lib/demo_web/live/table_live.ex
+++ lib/demo_web/live/table_live.ex
@@ -3,61 +3,125 @@ defmodule DemoWeb.TableLive do
 
   def render(assigns) do
     ~L"""
-    <div>
-      <a>Counter 1</a> | 
-      <a>Counter 2</a>
+    <div phx-keyup="press" phx-target="window">
+      <a phx-click="count" phx-value="counter1" >Counter 1</a> | 
+      <a phx-click="count" phx-value="counter2" >Counter 2</a>
       <hr/>
-      <form>
+      <form phx-submit="submit" phx-change="suggest">
         <input 
           type="text" 
           name="q" 
           placeholder="Search..." 
+          phx-focus="focus"
+          phx-blur="blur"
         />
       </form>
 
 
-      <table style="transform: rotate(0deg)" >
+      <table style="transform: rotate(0deg); <%= hide @focus %>" >
         <tr>
           <th>Name</th>
           <th>Value</th>
         </tr>
         <tr>
           <td>Socket.assigns.hello</td>
-          <td>?</td>
+          <td><%= @hello %></td>
         </tr>
         <tr>
           <td>Ticker</td>
-          <td>?</td>
+          <td><%= @clock %></td>
         </tr>
         <tr>
           <td>Counter1</td>
-          <td>?</td>
+          <td><%= @counter1 %></td>
         </tr>
         <tr>
           <td>Counter2</td>
-          <td>?</td>
+          <td><%= @counter2 %></td>
         </tr>
         <tr>
           <td>Search Field type</td>
-          <td>?</td>
+          <td><%= @typed %></td>
         </tr>
         <tr>
           <td>Search Field submit</td>
-          <td>?</td>
+          <td><%= @search %></td>
         </tr>
         <tr>
           <td>Search Field focus</td>
-          <td>?</td>
+          <td><%= @focus %></td>
         </tr>
       </table>
     </div>
     """
   end
   
+  def inc(socket, field) do
+    update(socket, field, &(&1 + 1))
+  end
+  
+  def press(socket, "1") do
+    inc(socket, :counter1)
+  end
+  def press(socket, "2") do
+    inc(socket, :counter2)
+  end
+  def press(socket, _other) do
+    socket
+  end
+  
+  def hide(true), do: "display: none;"
+  def hide(false), do: ""
+  
+  def search(socket, term) do
+    IO.inspect term
+    socket
+    |> assign(:search, "You searched for #{term}")
+  end
+
+  
   def mount(_session, socket) do
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    
     {
       :ok, 
       socket
+      |>assign(:hello, :world)
+      |>assign(:clock, 0)
+      |>assign(:counter1, 0)
+      |>assign(:counter2, 0)
+      |>assign(:typed, "--")
+      |>assign(:search, "--")
+      |>assign(:focus, false)
     }
   end
+  
+  def handle_info(:tick, socket) do
+    {:noreply, inc(socket, :clock)}
+  end
+  
+  def handle_event("count", field, socket) do
+    {:noreply, inc(socket, String.to_atom field)}
+  end
+
+  def handle_event("press", character, socket) do
+    {:noreply, press( socket, character )}
+  end
+
+  def handle_event("suggest", %{"q" => q}, socket) do
+    {:noreply, assign(socket, :typed, q)}
+  end
+
+  def handle_event("submit", %{"q" => q}, socket) do
+    {:noreply, search(socket, q)}
+  end
+  
+  def handle_event("focus", _, socket) do
+    {:noreply, assign(socket, :focus, true)}
+  end
+
+  def handle_event("blur", _, socket) do
+    {:noreply, assign(socket, :focus, false)}
+  end
+
 end
